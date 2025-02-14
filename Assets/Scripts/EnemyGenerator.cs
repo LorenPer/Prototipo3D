8ee81e3793e1f734a6e44e3c59f418b7ab1410cb
Lorenzo Pérez Blanes <lorperbla2@alu.edu.gva.es>
2025-02-14 09:51:35 +0100

    Update EnemyGenerator.cs

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -5,18 +5,19 @@ using UnityEngine;
 public class EnemyGenerator : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private Transform[] enemySpawners;
-    [SerializeField] private float spawnColdDown = 10;
+    [SerializeField] private GameObject enemyPrefab; //Objeto a generar
+    [SerializeField] private Transform[] enemySpawners; //Emptys donde se van a generar los enemigos
+    [SerializeField] private float spawnColdDown = 10; //Tiempo entre cada enemigo generado
 
     private void CreateEnemies()
     {
+        //Metodo para que se genere un enemigo ubicado en un empty aleatorio
         int n = Random.Range(0, enemySpawners.Length);
         Instantiate(enemyPrefab, enemySpawners[n].position, enemySpawners[n].rotation);
     }
     void Start()
     {
-        InvokeRepeating("CreateEnemies", 1.0f, spawnColdDown);
+        InvokeRepeating("CreateEnemies", 1.0f, spawnColdDown); //Invoke repeating para que se reactive el metodo para generar un enemigo
     }
 
     // Update is called once per frame
