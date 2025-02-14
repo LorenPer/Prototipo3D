7807aabcd3c1600dc3f5876b104ba7e28f2e2832
Lorenzo Pérez Blanes <lorperbla2@alu.edu.gva.es>
2025-02-14 10:10:50 +0100

    Update ProyectileManager.cs

--- Assets/Scripts/ProyectileManager.cs
+++ Assets/Scripts/ProyectileManager.cs
@@ -2,16 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//El proyectil funcionará distinto según su etiqueta.
 public class ProyectileManager : MonoBehaviour
 {
-    [SerializeField] private int speed = 100;
+    [SerializeField] private int speed = 100; //Velocidad del projectil
     void Update()
     {
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime); //El projectil se moverá a la velocidad indicada en deltaTime
     }
     void Start()
     {
-        Destroy(gameObject, 5);
+        Destroy(gameObject, 5); //El objeto se autodestruye tras pasar 5 segundos.
     }
 
 
